8576f169673998496ab679149d97d4d45658cd3b
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2023-12-17 19:59:16 +0700

    Remove Name from Signup and add Dialog when Signup

--- CapstoneTestApp/app/src/main/java/com/okta/capstonetestapp/ui/screen/signup/SignupScreen.kt
+++ CapstoneTestApp/app/src/main/java/com/okta/capstonetestapp/ui/screen/signup/SignupScreen.kt
@@ -6,17 +6,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,24 +16,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.okta.capstonetestapp.R
@@ -58,13 +41,13 @@ fun SignupScreen(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier,
 ) {
-    var inputNama by remember { mutableStateOf("") }
     var inputEmail by remember { mutableStateOf("") }
     var isEmailValid by remember { mutableStateOf(true) }
     var inputPassword by remember { mutableStateOf("") }
     var isPasswordTextTapped by remember { mutableStateOf(false) }
     val isPasswordValid = inputPassword.length >= 8
-    var output by remember { mutableStateOf("") }
+    val openDialog = remember { mutableStateOf(false) }
+    val signupResponse = remember { mutableStateOf(false) }
     val auth = FirebaseAuth.getInstance()
 
     database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://b4home-login-register-default-rtdb.firebaseio.com/")
@@ -90,25 +73,6 @@ fun SignupScreen(
             fontSize = 14.sp,
             modifier = Modifier
                 .padding(horizontal = 32.dp, vertical = 8.dp)
-        )
-        Text(
-            text = "Nama",
-            fontSize = 14.sp,
-            modifier = Modifier
-                .padding(start = 32.dp, end = 32.dp, top = 16.dp)
-        )
-        OutlinedTextField(
-            value = inputNama,
-            onValueChange = { newInput1 ->
-                inputNama = newInput1
-                output = newInput1
-            },
-            leadingIcon = { Icon(Icons.Outlined.Person, contentDescription = null) },
-            maxLines = 1,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 32.dp, end = 32.dp, bottom = 8.dp)
         )
         Text(
             text = "Email",
@@ -154,17 +118,14 @@ fun SignupScreen(
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             // User is successfully registered and logged in
-
+                            openDialog.value = true
+                            signupResponse.value = true
                         } else {
                             // Registration failed
-                            // ...
+                            openDialog.value = true
+                            signupResponse.value = false
                         }
                     }
-                navController.navigate(Screen.Login.route) {
-                    popUpTo(Screen.Welcome.route) { saveState = true }
-                    restoreState = true
-                    launchSingleTop = true
-                }
             },
             enabled = isEmailValid && inputEmail.isNotEmpty(),
             modifier = Modifier
@@ -175,6 +136,46 @@ fun SignupScreen(
             Text(
                 text = "Sign Up",
                 fontSize = 16.sp,
+            )
+        }
+        if (openDialog.value){
+            AlertDialog(
+                onDismissRequest = {
+                    openDialog.value = false
+                },
+                title = {
+                    if (signupResponse.value) {
+                        Text(text = "Yeah!")
+                    } else {
+                        Text(text = "Error!")
+                    }
+
+                },
+                text = {
+                    if (signupResponse.value) {
+                        Text("Successfully Register")
+                    } else {
+                        Text("Invalid")
+                    }
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            if (signupResponse.value) {
+                                openDialog.value = false
+                                navController.navigate(Screen.Login.route) {
+                                    popUpTo(Screen.Welcome.route) { saveState = true }
+                                    restoreState = true
+                                    launchSingleTop = true
+                                }
+                            } else {
+                                openDialog.value = false
+                            }
+                        }
+                    ) {
+                        Text("Lanjut")
+                    }
+                }
             )
         }
     }
